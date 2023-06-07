5abc70b03f371da29b510d7b4258cd3ceea7793c
JoshuaOliver8 <69984179+JoshuaOliver8@users.noreply.github.com>
2023-06-07 15:38:13 -0400

    ESLint

--- index.js
+++ index.js
@@ -10,154 +10,157 @@ app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('body', req => {
-    return JSON.stringify(req.body)
+	return JSON.stringify(req.body)
 })
 app.use(morgan(
-    ':method :url :status :res[content-length] - :response-time ms :body'
+	':method :url :status :res[content-length] - :response-time ms :body'
 ))
 
+/*
 let people = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+	{
+		"id": 1,
+		"name": "Arto Hellas",
+		"number": "040-123456"
+	},
+	{
+		"id": 2,
+		"name": "Ada Lovelace",
+		"number": "39-44-5323523"
+	},
+	{
+		"id": 3,
+		"name": "Dan Abramov",
+		"number": "12-43-234345"
+	},
+	{
+		"id": 4,
+		"name": "Mary Poppendieck",
+		"number": "39-23-6423122"
+	}
 ]
+*/
 
 app.get('/', (request, response) => {
-    response.send('<h1>Default</h1>')
+	response.send('<h1>Default</h1>')
 })
 
 app.get('/api/people', (request, response) => {
-    Person.find({}).then(people => {
-        response.json(people)
-    })
+	Person.find({}).then(people => {
+		response.json(people)
+	})
 })
 
 app.get('/info', (request, response, next) => {
-    const date = new Date()
+	const date = new Date()
 
-    Person.countDocuments({})
-        .then(count => {
-        if (count !== 1) {
-            response.send(`<p>
+	Person.countDocuments({})
+		.then(count => {
+			if (count !== 1) {
+				response.send(`<p>
                 Phonebook has info for ${count} people
                 <br></br>
                 ${date}
             </p>`)
-        } else {
-            response.send(`<p>
+			} else {
+				response.send(`<p>
                 Phonebook has info for ${count} person
                 <br></br>
                 ${date}
             </p>`)
-        }
-    })
-    .catch(error => next(error))
+			}
+		})
+		.catch(error => next(error))
 })
 
 app.get('/api/people/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-    })
-    .catch(error => next(error))
+	Person.findById(request.params.id)
+		.then(person => {
+			if (person) {
+				response.json(person)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
 
-app.delete('/api/people/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+app.delete('/api/people/:id', (request, response, next) => {
+	Person.findByIdAndRemove(request.params.id)
+		// eslint-disable-next-line no-unused-vars
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 //const generateID = () => Math.floor(Math.random() * 1000000000)
 
 app.post('/api/people', (request, response, next) => {
-    const body = request.body
+	const body = request.body
 
-    Person.findOne({ name: body.name })
-        .then(p => {
-            if (p) {
-                const person = {
-                    name: body.name,
-                    number: body.number,
-                }
+	Person.findOne({ name: body.name })
+		.then(p => {
+			if (p) {
+				const person = {
+					name: body.name,
+					number: body.number,
+				}
 
-                Person.findByIdAndUpdate(p.id, person, { new: true })
-                    .then(updatedPerson => {
-                        response.json(updatedPerson)
-                    })
-                    .catch(error => next(error))
-            } else {
-                const person = new Person({
-                    name: body.name,
-                    number: body.number,
-                })
-            
-                person.save()
-                    .then(savedPerson => {
-                        response.json(savedPerson)
-                    })
-                    .catch(error => next(error))
-            }
-        })
+				Person.findByIdAndUpdate(p.id, person, { new: true })
+					.then(updatedPerson => {
+						response.json(updatedPerson)
+					})
+					.catch(error => next(error))
+			} else {
+				const person = new Person({
+					name: body.name,
+					number: body.number,
+				})
+
+				person.save()
+					.then(savedPerson => {
+						response.json(savedPerson)
+					})
+					.catch(error => next(error))
+			}
+		})
 })
 
 app.put('/api/people/:id', (request, response, next) => {
-    const { name, number } = request.body
+	const { name, number } = request.body
 
-    Person.findByIdAndUpdate(
-        request.params.id,
-        { name, number },
-        { new: true, runValidators: true, context: 'query' })
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+	Person.findByIdAndUpdate(
+		request.params.id,
+		{ name, number },
+		{ new: true, runValidators: true, context: 'query' })
+		.then(updatedPerson => {
+			response.json(updatedPerson)
+		})
+		.catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+	console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformed id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformed id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
 
-    next(error)
+	next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -7,42 +7,43 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+	// eslint-disable-next-line no-unused-vars
+	.then(result => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: (number => {
-                if (/^\d{2,3}-\d{1,}$/.test(number)){
-                    return true
-                } else {
-                    return false
-                }
-            }),
-            message: 'phone number must consist of two parts separated by -, be formed of two parts that are separated by -, the first part has two or three numbers and the second part also consists of numbers'
-        },
-        required: true
-    },
+	name: {
+		type: String,
+		minLength: 3,
+		required: true
+	},
+	number: {
+		type: String,
+		minLength: 8,
+		validate: {
+			validator: (number => {
+				if (/^\d{2,3}-\d{1,}$/.test(number)){
+					return true
+				} else {
+					return false
+				}
+			}),
+			message: 'phone number must consist of two parts separated by -, be formed of two parts that are separated by -, the first part has two or three numbers and the second part also consists of numbers'
+		},
+		required: true
+	},
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
 module.exports = mongoose.model('Person', personSchema)
